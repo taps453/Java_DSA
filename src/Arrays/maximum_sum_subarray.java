753c41520c5e7a3d1dcfe8bd1df8e80e99d5ff10
taps453 <taps453@gmail.com>
2025-08-23 00:05:37 +0530

    code update

--- src/Arrays/maximum_sum_subarray.java
+++ src/Arrays/maximum_sum_subarray.java
@@ -5,11 +5,12 @@ public class maximum_sum_subarray {
 	public static void main(String[] args) {
 		int[] arr = {-2,1,-3,4,-1,2,1,-5,4}; // output = 6
 		int[] arr1 = {2, 3, -8, 7, -1, 2, 3}; // output = 11
-		System.out.println(max_sum(arr1));
+		System.out.println(max_sum(arr));
+		System.out.println(max_sum1(arr1));
 	}
 	
 	public static int max_sum(int[] arr) {
-		int n = arr.length -1;
+		int n = arr.length;
 		int sum = arr[0];		
 		for(int i = 0; i < n; i++) {
 			int new_sum  = 0;
@@ -19,5 +20,16 @@ public class maximum_sum_subarray {
 			}
 		}
 		return sum;
+	}
+	
+	public static int max_sum1(int[] arr){
+		
+		int cur_sum = arr[0], total_sum = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			cur_sum = Math.max(cur_sum + arr[i] , arr[i]);
+			total_sum = Math.max(total_sum, cur_sum);
+		}
+		return total_sum;
+		
 	}
 }
