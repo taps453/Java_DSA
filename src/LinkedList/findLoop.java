92fb5472a3d8bd6cb92c6976766c111d1b7619f3
taps453 <taps453@gmail.com>
2025-09-05 23:58:20 +0530

    list code updated

--- src/LinkedList/findLoop.java
+++ src/LinkedList/findLoop.java
@@ -1,5 +1,8 @@
 package LinkedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class findLoop {
 
 	public static void main(String[] args) {
@@ -9,8 +12,8 @@ public class findLoop {
 		head.next.next.next = new Node(40);
 		head.next.next.next.next = new Node(50);
 		head.next.next.next.next.next = head.next;
-		System.out.println(loopFind(head));
-
+//		System.out.println(loopFind(head));
+		System.out.println(loopfind1(head));
 	}
 	
 	public static boolean loopFind(Node head) {
@@ -23,6 +26,20 @@ public class findLoop {
 			if(slow == fast) {
 				return true;
 			}
+		}
+		return false;
+	}
+
+	public static boolean loopfind1(Node head) {
+		Set<Node> set = new HashSet<Node>();
+		Node cur = head;
+		
+		while(cur != null) {
+			if(set.contains(cur)) {
+				return true;
+			}
+			set.add(cur);
+			cur = cur.next;
 		}
 		return false;
 	}
